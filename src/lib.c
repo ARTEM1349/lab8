c5c78ca7544ea085a5a5b0abc29d353114a5a203
ARTEM1349 <64367956+ARTEM1349@users.noreply.github.com>
2022-11-28 14:05:26 +0200

    Create lib.c

---
+++ src/lib.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+int lab06(long int b[], int s) 
+{
+    
+    long int i,j,c;
+    for ( i = 0; i<s - 1;i++)
+     {
+        for ( j  =0;j<s-i-1;j++)
+         {
+            if (b[j] > b[j+1])
+            {
+                c =b[j];
+                b[j] = b[j+1];
+                b[j+1] = c;
+            }
+       }
+       
+    } 
+    for(i = 0; i < s; i++)
+        printf ("%ld\n",b[i]);
+        
+    return 0;
+}
+
+int lab05(a)
+{
+     int i;
+     
+     for (i = 1; a>0 ; a--) 
+     {
+     printf ("%d-",a);
+       i= i*a;
+       printf ("%d\n",i);
+   } 
+   printf ("\n");
+    return 0;
+}
